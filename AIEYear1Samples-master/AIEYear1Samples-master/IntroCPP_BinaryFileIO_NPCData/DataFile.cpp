9c29a914e981cece13d8532be44a9cb4d4972e32
Terrasean123 <makokatinse@gmail.com>
2023-08-23 10:08:22 +1000

    Update DataFile.cpp

--- AIEYear1Samples-master/AIEYear1Samples-master/IntroCPP_BinaryFileIO_NPCData/DataFile.cpp
+++ AIEYear1Samples-master/AIEYear1Samples-master/IntroCPP_BinaryFileIO_NPCData/DataFile.cpp
@@ -21,7 +21,7 @@ void DataFile::AddRecord(string imageFilename, string name, int age)
 	r->name = name;
 	r->age = age;
 
-	
+
 	recordCount++;
 }
 
@@ -31,7 +31,7 @@ DataFile::Record* DataFile::GetRecord(int index, std::string filename)// because
 
 	infile.read((char*)&recordCount, sizeof(int));
 
-	
+
 
 	for (int i = 0; i < index; i++)
 	{
@@ -46,24 +46,11 @@ DataFile::Record* DataFile::GetRecord(int index, std::string filename)// because
 
 		infile.read((char*)&nameSize, sizeof(int));
 		infile.read((char*)&ageSize, sizeof(int));
+		infile.seekg(nameSize + ageSize + imageSize,ios_base::cur);
 
-		char* imgdata = new char[imageSize];
-		infile.read(imgdata, imageSize);
-
-		Image img = LoadImageEx((Color*)imgdata, width, height);
-		char* name = new char[nameSize]();
-		int age = 0;
-
-		infile.read((char*)name, nameSize);
-		infile.read((char*)&age, ageSize);
-
-		 //previously the constructor used copied the character sequence assuming it was already null terminated
-		        		  //The fact that  our char sequence wasnt null terminated was causing the the script to overread.
-		//the new constructor uses the name size to clarify where the  code should stop reading ;
-
-		delete[] imgdata;
-		delete[] name;
-
+		//previously the constructor used copied the character sequence assuming it was already null terminated
+	  //The fact that  our char sequence wasnt null terminated was causing the the script to overread.
+	   //the new constructor uses the name size to clarify where the  code should stop reading ;
 	}
 
 	int nameSize = 0;
@@ -101,93 +88,4 @@ DataFile::Record* DataFile::GetRecord(int index, std::string filename)// because
 	infile.close();
 
 	return currentRecord; // return the record required
-}
-
-//void DataFile::Save(string filename)
-//{
-//	ofstream outfile(filename, ios::binary);
-//
-//	int recordCount = records.size();
-//	outfile.write((char*)&recordCount, sizeof(int));
-//
-//	for (int i = 0; i < recordCount; i++)
-//	{
-//		Color* imgdata = GetImageData(records[i]->image);
-//
-//		int imageSize = sizeof(Color) * records[i]->image.width * records[i]->image.height;
-//		int nameSize = records[i]->name.length();
-//		int ageSize = sizeof(int);
-//
-//		outfile.write((char*)&records[i]->image.width, sizeof(int));
-//		outfile.write((char*)&records[i]->image.height, sizeof(int));
-//
-//		outfile.write((char*)&nameSize, sizeof(int));
-//		outfile.write((char*)&ageSize, sizeof(int));
-//
-//		outfile.write((char*)imgdata, imageSize);
-//		outfile.write((char*)records[i]->name.c_str(), nameSize);
-//		outfile.write((char*)&records[i]->age, ageSize);
-//	}
-//
-//	outfile.close();
-//}
-
-//void DataFile::Load(string filename)
-//{
-//	Clear();
-//
-//	ifstream infile(filename, ios::binary);
-//
-//	recordCount = 0;
-//	infile.read((char*)&recordCount, sizeof(int));
-//
-//
-//	int currentIndex = infile.tellg();
-//
-//	for (int i = 0; i < recordCount; i++)
-//	{
-//		recordPositions.push_back(infile.tellg());// record index positions saved here
-//
-//		int nameSize = 0;
-//		int ageSize = 0;
-//		int width = 0, height = 0, format = 0, imageSize = 0;
-//
-//		infile.read((char*)&width, sizeof(int));
-//		infile.read((char*)&height, sizeof(int));
-//
-//		imageSize = sizeof(Color) * width * height;
-//
-//		infile.read((char*)&nameSize, sizeof(int));
-//		infile.read((char*)&ageSize, sizeof(int));
-//
-//		char* imgdata = new char[imageSize];
-//		infile.read(imgdata, imageSize);
-//
-//		Image img = LoadImageEx((Color*)imgdata, width, height);
-//		char* name = new char[nameSize]();
-//		int age = 0;
-//
-//		infile.read((char*)name, nameSize);
-//		infile.read((char*)&age, ageSize);
-//
-//		Record* r = new Record();
-//		r->image = img;
-//		r->name = string(name, nameSize);// This was using the incorrect constructor; previously the constructor used copied the character sequence assuming it was already null terminated.
-//		//The fact that  our char sequence wasnt null terminated was causing the the script to overread.
-//	    //the new constructor uses the name size to clarify where the  code should stop reading  
-//		r->age = age;
-//
-//		delete[] imgdata;
-//		delete[] name;
-//	}
-//
-//	infile.close();
-//}
-
-
-void DataFile::Clear()
-{
-
-	//records.clear();
-	recordCount = 0;
 }
